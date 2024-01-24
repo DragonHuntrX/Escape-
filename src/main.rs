49aae4986792225f15ea9f3ef2972dc37d457b35
DragonHuntrX <John@Devost.net>
2024-01-24 16:42:45 -0500

    New

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Player;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -9,4 +12,13 @@ fn main() {
 
 fn spawn_camera_system(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_player_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut player_image: Handle<Texture> = asset_server.load("playersprite.png");
+
+    commands.spawn(SpriteBundle {
+        transform: Transform::from_xyz(100., 0., 0.),
+        ..default()
+    });
 }
